10bf825965688c46cd475f72a0c506e27480478e
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-03-06 11:00:38 +0700

    Add Cipayung

--- scrap.py
+++ scrap.py
@@ -5,7 +5,7 @@ import json
 
 idKec = 26028
 
-url = "http://dom.garbiapp.com/kpu/wilayah/?op=wilayah&q=26126&lev=4&parent=26065"
+url = "http://dom.garbiapp.com/kpu/wilayah/?op=wilayah&q=26132&lev=4&parent=26065"
 res = requests.get(url)
 soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0] 
