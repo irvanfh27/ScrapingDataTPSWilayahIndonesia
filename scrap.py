c796413022c458865e40f6699404d8287dc38561
Irvan Fathul Huda <irvanfathul@gmail.com>
2019-03-06 10:42:37 +0700

    Add Pancoran

--- scrap.py
+++ scrap.py
@@ -5,7 +5,7 @@ import json
 
 idKec = 26028
 
-url = "http://dom.garbiapp.com/kpu/wilayah/?op=wilayah&q=26034&lev=4&parent=25989"
+url = "http://dom.garbiapp.com/kpu/wilayah/?op=wilayah&q=26045&lev=4&parent=25989"
 res = requests.get(url)
 soup = BeautifulSoup(res.content,'lxml')
 table = soup.find_all('table')[0] 
